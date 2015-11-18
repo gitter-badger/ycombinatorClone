34deba7b8cdefb624e682c94f9130d48ca4032f9
Benson Mburu <nguruben@gmail.com>
2015-11-18 18:26:36 +0300

    added more models

--- clone/models.py
+++ clone/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Forum(models.Model):
+	linka = models.CharField(max_length=100, unique=False)
+	linkb = models.CharField(max_length=100, unique=False)
+	author = models.CharField(max_length=100, unique=False)
 	title = models.CharField(max_length=100, unique=True)
 	link = models.CharField(max_length=100, unique=False)
 	votes= models.CharField(max_length=100, unique=False)
