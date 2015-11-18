43d43b252a1b98970e68206400e82878103aa41f
Benson Mburu <nguruben@gmail.com>
2015-11-18 11:07:34 +0300

    views added

--- clone/views.py
+++ clone/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
 
+from models import Forum
 # Create your views here.
+def index(request):
+	return render_to_response('index.html'{
+		'Title':Title.objects.all(),
+		'Link':Link.objects.all(),
+		'Votes':Link.objects.all(),
+		'Time':Link.objects.all(),
+		'Comments': Comment.objects.all(),
+	})
+
