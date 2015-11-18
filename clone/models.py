b6377dc644f249a67bbbf93a334cbb681aa6a793
Benson Mburu <nguruben@gmail.com>
2015-11-18 10:55:38 +0300

    working models

--- clone/models.py
+++ clone/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Forum(models.Model):
-	Title = models.CharField(max_lenght=100, unique=True)
-	Link = models.CharField(max_lenght=100, unique=False)
-	Votes= modesl.CharField(max_lenght=100, unique=False)
+	Title = models.CharField(max_length=100, unique=True)
+	Link = models.CharField(max_length=100, unique=False)
+	Votes= models.CharField(max_length=100, unique=False)
 	Time = models.TimeField(db_index=True, auto_now_add=True)
-	Slug = models.SlugField(max_lenght=100, unique=True)
+	Slug = models.SlugField(max_length=100, unique=True)
 
-	def __unicode__(self)
-	return '%s' %self.Title
+	def __unicode__(self):
+		return '%s' %self.Title
